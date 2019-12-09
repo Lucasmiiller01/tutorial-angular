94bbdbca95b79881ed47c7a05f8f40eaeaa1242c
LUCAS DA SILVA MIILLER CORREA <lcorrea.terceiro@brasilcap.com.br>
2019-12-09 11:22:39 -0300

    Separando sem servicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,30 @@
 import { Component } from '@angular/core';
+import { HeroService } from './services/hero.service';
+import { Hero } from './Models/hero';
 
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
+
 })
 export class AppComponent {
+
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[];
   selectedHero: Hero;
-  heroes = HEROES;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+    
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     console.log(hero);
